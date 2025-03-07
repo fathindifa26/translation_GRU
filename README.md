70227c343cf130ad521cc2794aa041cf42b94cd6
Fathin Difa Robbani <difarobbani267@gmail.com>
2025-03-07 17:46:33 +0700

    menambahkan yaml

--- README.md
+++ README.md
@@ -4,5 +4,16 @@
 1. Install dependencies:
    ```bash
    pip install -r requirements.txt
+   ```
+
+
+# Run
+```bash
+conda activate gru_translation
 
 export PATH="/opt/homebrew/anaconda3/envs/gru_translation/bin:$PATH"
+
+cd src/experiments/
+
+python train_gru.py
+```
--- src/experiments/train_gru.py
+++ src/experiments/train_gru.py
@@ -8,6 +8,7 @@ import torchtext
 import tqdm
 import sys
 import os
+import yaml
 # Tambahkan path ke src agar bisa mengakses data/
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
@@ -18,10 +19,13 @@ from utils.training import init_model, train_fn, evaluate_fn
 from datasets import load_from_disk
 
 
+# file yaml
+with open(os.path.abspath("../../configs/gru_seq2seq.yaml"), "r") as f:
+    config = yaml.safe_load(f)
+
 
 # import file
-import os
-dataset_path = os.path.abspath("../../data/indonesia_jawa_dataset")
+dataset_path = os.path.abspath(config["data"]["dataset_path"])
 print(f"Dataset path: {dataset_path}")
 
 dataset = load_from_disk(dataset_path)
@@ -38,27 +42,38 @@ train_data, valid_data, test_data, en_vocab, id_vocab = preprocess_dataset(datas
 
 
 # data loader
-pad_index = en_vocab["<pad>"]
-batch_size = 128  
+pad_index = en_vocab[config["data"]["pad_token"]]
+batch_size = config["training"]["batch_size"]
 train_loader = get_data_loader(train_data, batch_size=batch_size, pad_index=pad_index, shuffle=True)
 valid_loader = get_data_loader(valid_data, batch_size=batch_size, pad_index=pad_index, shuffle=False)
 test_loader = get_data_loader(test_data, batch_size=batch_size, pad_index=pad_index, shuffle=False)
 
 
-# TRAIN
-input_dim = len(id_vocab)
-output_dim = len(en_vocab)
-embedding_dim = 512
-hidden_dim = 1024
-dropout = 0.3
-clip = 1.0
-teacher_forcing_initial = 0.5
-teacher_forcing_final = 0.1
-epochs = 10
+# Inisiasi model
+config["model"]["input_dim"] = len(id_vocab)
+config["model"]["output_dim"] = len(en_vocab)
+
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {device}")
 
-model, optimizer, criterion = init_model(input_dim, output_dim, embedding_dim, hidden_dim, dropout, pad_index, device)
+model, optimizer, criterion = init_model(
+    config["model"]["input_dim"],
+    config["model"]["output_dim"],
+    config["model"]["embedding_dim"],
+    config["model"]["hidden_dim"],
+    config["model"]["dropout"],
+    pad_index,
+    device
+)
+
+epochs = config["training"]["epochs"]
+clip = config["training"]["clip"]
+teacher_forcing_initial = config["training"]["teacher_forcing_initial"]
+teacher_forcing_final = config["training"]["teacher_forcing_final"]
+checkpoint_path = config["training"]["checkpoint_path"]
+patience = config["training"]["patience"]
+patience_counter = config["training"]["patience_counter"]
+
 
 # Training Loop
 best_valid_loss = float("inf")
