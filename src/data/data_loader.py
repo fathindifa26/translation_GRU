d79632cec1528904a24c7077523df5ebdf01f267
Fathin Difa Robbani <difarobbani267@gmail.com>
2025-03-07 16:54:56 +0700

    preprocessing.py dan data_loader.py aman

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -6,16 +6,23 @@ def get_collate_fn(pad_index):
     def collate_fn(batch):
         batch_en_ids = [example["en_ids"] for example in batch]
         batch_id_ids = [example["id_ids"] for example in batch]
-        batch_en_ids = nn.utils.rnn.pad_sequence(batch_en_ids, padding_value=pad_index)
-        batch_id_ids = nn.utils.rnn.pad_sequence(batch_id_ids, padding_value=pad_index)
-        return {"en_ids": batch_en_ids, "id_ids": batch_id_ids}
+        batch_en_ids = nn.utils.rnn.pad_sequence(batch_en_ids, padding_value=pad_index) # menambah padding
+        batch_id_ids = nn.utils.rnn.pad_sequence(batch_id_ids, padding_value=pad_index) # menambah padding
+        batch = {
+            "en_ids": batch_en_ids,
+            "id_ids": batch_id_ids,
+        }
+        return batch
+
     return collate_fn
 
 def get_data_loader(dataset, batch_size, pad_index, shuffle=False):
     collate_fn = get_collate_fn(pad_index)
-    return torch.utils.data.DataLoader(
+    data_loader = torch.utils.data.DataLoader(
         dataset=dataset,
         batch_size=batch_size,
         collate_fn=collate_fn,
         shuffle=shuffle,
     )
+
+    return data_loader
